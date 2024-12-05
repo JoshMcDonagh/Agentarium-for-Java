fdfd12f2d3dbee5066932a9fc604abe5ec59874f
JoshMcDonagh <jmmarland99@gmail.com>
2024-12-05 18:49:37 +0000

    Add RequestResponseOperator

--- src/main/java/models/multithreading/requestresponse/RequestResponseOperator.java
+++ src/main/java/models/multithreading/requestresponse/RequestResponseOperator.java
@@ -1,8 +1,15 @@
 package models.multithreading.requestresponse;
 
+import agents.Agent;
 import models.ModelClock;
+import models.modelattributes.ModelAttributeSet;
+import models.multithreading.threadutilities.AgentStore;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.BlockingQueue;
+import java.util.function.Predicate;
 
 public class RequestResponseOperator {
     private String threadName;
@@ -26,15 +33,68 @@ public class RequestResponseOperator {
         requestQueue.put(new Request(threadName, null, requestCode, null));
 
         while (true) {
-            while (responseQueue.isEmpty())
-                continue;
+            while (responseQueue.isEmpty());
 
             Response response = responseQueue.poll();
 
-            if (response.getResponseCode() == responseCode && response.getDestination() == threadName)
+            if (Objects.equals(response.getResponseCode(), responseCode) && Objects.equals(response.getDestination(), threadName))
                 return;
             else
                 responseQueue.put(response);
         }
+    }
+
+    public void waitUntilAllWorkersFinishTick() throws InterruptedException {
+        wait(RequestCodes.ALL_WORKERS_FINISH_TICK, ResponseCodes.ALL_WORKERS_FINISH_TICK);
+    }
+
+    public void waitUntilAllWorkersUpdateCoordinator() throws InterruptedException {
+        wait(RequestCodes.UPDATE_COORDINATOR_AGENTS, ResponseCodes.ALL_WORKERS_UPDATE_COORDINATOR);
+    }
+
+    public Agent getAgentFromCoordinator(String requesterAgentName, String targetAgentName) throws InterruptedException {
+        requestQueue.put(new Request(requesterAgentName, targetAgentName, RequestCodes.AGENT_ACCESS, null));
+
+        while (true) {
+            while (responseQueue.isEmpty());
+
+            Response response = responseQueue.poll();
+
+            if (Objects.equals(response.getResponseCode(), ResponseCodes.AGENT_ACCESS) && Objects.equals(response.getDestination(), requesterAgentName))
+                return (Agent) response.getPayload();
+            responseQueue.put(response);
+        }
+    }
+
+    public List<Agent> getFilteredAgentsFromCoordinator(String requesterAgentName, Predicate<Agent> agentFilter) throws InterruptedException {
+        requestQueue.put(new Request(requesterAgentName, null, RequestCodes.FILTERED_AGENTS_ACCESS, null));
+
+        while (true) {
+            while (responseQueue.isEmpty());
+
+            Response response = responseQueue.poll();
+
+            if (Objects.equals(response.getResponseCode(), ResponseCodes.FILTERED_AGENTS_ACCESS) && Objects.equals(response.getDestination(), requesterAgentName))
+                return (List<Agent>) response.getPayload();
+            responseQueue.put(response);
+        }
+    }
+
+    public Map<String, ModelAttributeSet> getModelAttributesFromCoordinator(String requesterAgentName) throws InterruptedException {
+        requestQueue.put(new Request(requesterAgentName, null, RequestCodes.MODEL_ATTRIBUTES_ACCESS, null));
+
+        while (true) {
+            while (responseQueue.isEmpty());
+
+            Response response = responseQueue.poll();
+
+            if (Objects.equals(response.getResponseCode(), ResponseCodes.MODEL_ATTRIBUTES_ACCESS) && Objects.equals(response.getDestination(), requesterAgentName))
+                return (Map<String, ModelAttributeSet>) response.getPayload();
+            responseQueue.put(response);
+        }
+    }
+
+    public void updateCoordinatorAgents(AgentStore agentStore) throws InterruptedException {
+        requestQueue.put(new Request(null, null, RequestCodes.UPDATE_COORDINATOR_AGENTS, agentStore));
     }
 }

3a30167a98b5a8dc43e196fd78b2ed6088999639
JoshMcDonagh <jmmarland99@gmail.com>
2025-11-18 23:29:40 +0000

    Rename integration tests

---
+++ src/test/java/integration/agentEnvironmentSyncIntegrationTest/attributes/ModelAttributes.java
@@ -0,0 +1,42 @@
+package integration.agentEnvironmentSyncIntegrationTest.attributes;
+
+import agentarium.attributes.AttributeSet;
+import agentarium.attributes.AttributeSetCollection;
+import agentarium.attributes.Events;
+import agentarium.attributes.Properties;
+import integration.agentEnvironmentSyncIntegrationTest.attributes.environment.properties.EnvTick;
+import integration.agentEnvironmentSyncIntegrationTest.attributes.agent.properties.SeenEnvTick;
+
+public class ModelAttributes {
+
+    public static AttributeSetCollection getAgentAttributeSetCollection() {
+        // Empty pre/post event lists (same pattern as test1/test2)
+        Events preEvents = new Events();
+        Events postEvents = new Events();
+
+        // Add the property to the Properties collection (use add, not put)
+        Properties properties = new Properties();
+        properties.add(new SeenEnvTick());
+
+        // Build the attribute set and add it to the collection (use add, not addAttributeSet)
+        AttributeSet perception = new AttributeSet("perception", preEvents, properties, postEvents);
+
+        AttributeSetCollection c = new AttributeSetCollection();
+        c.add(perception);
+        return c;
+    }
+
+    public static AttributeSetCollection getEnvironmentAttributeSetCollection() {
+        Events preEvents = new Events();
+        Events postEvents = new Events();
+
+        Properties properties = new Properties();
+        properties.add(new EnvTick());
+
+        AttributeSet climate = new AttributeSet("climate", preEvents, properties, postEvents);
+
+        AttributeSetCollection c = new AttributeSetCollection();
+        c.add(climate);
+        return c;
+    }
+}
